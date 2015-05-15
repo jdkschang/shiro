d19da56bfbfdb5c2d709153e5a7ea278d5766411
Jonathan Chang <jdkschang@gmail.com>
2015-05-14 19:45:33 -0700

    removed unecessary files for shirou-v2

--- js/angular.js
+++
@@ -1,9 +0,0 @@
-var myApp = angular.module('myApp', []);
-
-myApp.controller('MainCtrl', ['$scope', function ($scope) {
-  $scope.text = 'Hello, Dmitri.';
-}]);
-
-// myApp.controller('NavCtrl', ['$scope', function ($scope) {...}]);
-
-// myApp.controller('UserCtrl', ['$scope', function ($scope) {...}]);
