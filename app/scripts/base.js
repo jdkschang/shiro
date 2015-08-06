ae9d2219405483f547e5178c9c4294ebb9f88ca9
Jonathan Chang <jdkschang@gmail.com>
2015-08-05 19:44:07 -0700

    m | .eslintrc able to console and alert statements

--- app/scripts/base.js
+++ app/scripts/base.js
@@ -1,3 +1,3 @@
 // base.js
-alert("hello, my name i shirou v2.0");
-// console.log('hello, my name is shirou v2.0');
+// alert("hello, my name i shirou v2.0");
+ console.log("hello, my name is shirou v2.0");
