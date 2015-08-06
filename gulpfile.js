de51b23fa2b21858d256684da04bf7ea955971c3
Jonathan Chang <jdkschang@gmail.com>
2015-08-05 17:25:07 -0700

    m | gulpfile.js eslint integration over jshint

--- gulpfile.js
+++ gulpfile.js
@@ -26,16 +26,23 @@ gulp.task('clean', function() {
 });
 
 gulp.task('lint', function() {
-  return gulp.src('client/app/**/*.js')
-    .pipe(eslint())
-    .pipe(eslint.format());
+    return gulp.src(paths.scripts, {cwd: bases.app})
+          // eslint() attaches the lint output to the eslint property
+          // of the file object so it can be used by other modules.
+          .pipe(eslint())
+          // eslint.format() outputs the lint results to the console.
+          // Alternatively use eslint.formatEach() (see Docs).
+          .pipe(eslint.format())
+          // To have the process exit with an error code (1) on
+          // lint error, return the stream and pipe to failOnError last.
+          .pipe(eslint.failOnError());
 });
 
 // process scripts & concatenate into output file
 gulp.task('scripts', ['clean'], function() {
   gulp.src(paths.scripts, {cwd: bases.app})
-      .pipe(jshint())
-      .pipe(jshint.reporter('default'))
+      .pipe(eslint())
+      .pipe(eslint.format())
       .pipe(uglify())
       .pipe(concat('app.min.js'))
       .pipe(gulp.dest(bases.dist + 'scripts/'));
