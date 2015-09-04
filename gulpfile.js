c8aa98533e8b67b349b4f923145cad150b589c08
Jonathan Chang <jdkschang@gmail.com>
2015-09-03 21:12:08 -0700

    m | gulpfile.js complete

--- gulpfile.js
+++ gulpfile.js
@@ -21,22 +21,14 @@ var gulp    		= require('gulp'),
 	cp 				= require('child_process'),
 	browserSync 	= require('browser-sync');
 
-// var bases   = {
-//                 app: 'app/',
-//                 dist: 'dist/'
-//               };
-
-
 // PATHS
 var paths 	= {
 	distFolder: 			'./public/dist/',
 	mainStyleFile: 			'./public/src/main.scss',
 	componentStyleFiles:	'./public/src/components/**/*.scss',
-	themeStyleFiles: 		'./public/src/theme/**/*.scss',
 	srcStyleFiles: [
 		mainStyleFile,
-		componentStyleFiles,
-		themeStyleFiles
+		componentStyleFiles
 	],
 	mainScriptFile: 		'./public/src/main.js',
 	componentScriptFiles: 	'./public/src/components/**/*.js',
@@ -46,15 +38,6 @@ var paths 	= {
 	],
 	srcImageFiles: 			['./public/src/images/**/*.{gif,jpg,png,svg}'],
 	srcFontFiles: 			['./public/src/fonts/**/*.{ttf,woff,eot,svg}'],
-	srcMarkdownFiles: [
-		'!_site/**/*',
-		'*/*.html',
-		'*/*.md',
-		'_includes/*.html',
-		'_layouts/*.html',
-		'_posts/*.md',
-		'_config.yml'
-	]
 };
 
 
@@ -86,67 +69,144 @@ var customSassError = function (err) {
 };
 
 
-// clean dist directory
-gulp.task('clean:dist', function( callback ) {
-    del([
-        'dist/styles/*',
-        // here we use a globbing pattern to match everything inside the `mobile` folder
-        // 'dist/mobile/**/*',
-        // we don't want to clean this file though so we negate the pattern
-        // '!dist/mobile/deploy.json'
-    ], cb);
+
+//////////////////////////////////////////////////
+// TASKS
+//////////////////////////////////////////////////
+
+// GENERAL
+gulp.task('default', 'assets');
+gulp.task('assets', ['styles', 'scripts', 'images', 'fonts']);
+gulp.task('serve', ['default', 'browser-sync', 'watch']);
+
+
+// STYLES
+gulp.task('styles', function () {
+	return gulp.src(paths.mainStyleFile)
+		.pipe(plumber({errorHandler: onError}))
+		.pipe(sourcemaps.init())
+		.pipe(sass({onError: customSassError}))
+		.pipe(concat('all.min.css'))
+		.pipe(cmq())
+		.pipe(autoprefixer({browsers: ['last 2 versions']}))
+		.pipe(minifyCSS())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(paths.distFolder))
+		.pipe(gulp.dest('_site/public/dist/'));
 });
 
-gulp.task('lint', function() {
-    return gulp.src(paths.scripts, {cwd: bases.app})
-              // eslint() attaches the lint output to the eslint property
-              // of the file object so it can be used by other modules.
-              .pipe(eslint())
-              // eslint.format() outputs the lint results to the console.
-              // Alternatively use eslint.formatEach() (see Docs).
-              .pipe(eslint.format())
-              // To have the process exit with an error code (1) on
-              // lint error, return the stream and pipe to failOnError last.
-              .pipe(eslint.failOnError());
+// SCRIPTS
+gulp.task('scripts', function(){
+	return gulp.src(paths.srcScriptFiles)
+		.pipe(plumber({errorHandler: onError}))
+		.pipe(sourcemaps.init())
+		.pipe(jshint('.jshintrc'))
+		.pipe(jshint.reporter(stylish))
+		.pipe(jshint.reporter('fail'))
+		.pipe(moduleTranspiler({formatter: 'bundle'}))
+		.pipe(babel())
+		.pipe(concat('all.min.js'))
+		.pipe(uglify())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(paths.distFolder))
+		.pipe(gulp.dest('_site/public/dist/'));
 });
 
-// process scripts & concatenate into output file
-gulp.task('scripts', ['clean'], function() {
-    gulp.src(paths.scripts, {cwd: bases.app})
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(uglify())
-        .pipe(concat('app.min.js'))
-        .pipe(gulp.dest(bases.dist + 'scripts/'));
+// IMAGES
+gulp.task('images', function (){
+	return gulp.src(paths.srcImageFiles)
+		.pipe(plumber({errorHandler: onError}))
+		.pipe(imagemin())
+		.pipe(gulp.dest(paths.distFolder + 'images'));
 });
 
-// imagemin minimizes images and outputs to dist
-// gulp.task('imgmin', ['clean'], function() {
-//     gulp.src(paths.images, {cwd: bases.app})
-//         .pipe(imgmin())
-//         .pipe(gulp.dest(bases.dist + 'images/'));
+// FONTS
+gulp.task('fonts', function() {
+	return gulp.src(paths.srcFontFiles)
+		.pipe(plumber({errorHandler: onError}))
+		.pipe(gulp.dest(paths.distFolder + 'fonts'));
+});
+
+// BROWSERSYNC
+gulp.task('browser-sync', ['default'], function() {
+	browserSync({
+		server: {
+			baseDir: '_site'
+		},
+		open: 'ui'
+	});
+});
+
+// WATCH
+gulp.task('watch', ['default'], function () {
+	gulp.watch(paths.srcStyleFiles, ['styles', browserSync.reload]);
+	gulp.watch(paths.srcScriptFiles, ['scripts', browserSync.reload]);
+	gulp.watch(paths.srcImageFiles, ['images', browserSync.reload]);
+	gulp.watch(paths.srcFontFiles, ['fonts', browserSync.reload]);
+	gulp.watch(paths.srcMarkdownFiles, ['markdown', browserSync.reload]);
+});
+
+// // clean dist directory
+// gulp.task('clean:dist', function( callback ) {
+//     del([
+//         'dist/styles/*',
+//         // here we use a globbing pattern to match everything inside the `mobile` folder
+//         // 'dist/mobile/**/*',
+//         // we don't want to clean this file though so we negate the pattern
+//         // '!dist/mobile/deploy.json'
+//     ], cb);
 // });
-
-// copy all other files to dist directly
-gulp.task('copy', ['clean'], function() {
-    // copy html
-    gulp.src(paths.html, {cwd: bases.app})
-        .pipe(gulp.dest(bases.dist));
-
-    // copy styles
-    gulp.src(paths.styles, {cwd: bases.app})
-        .pipe(gulp.dest(bases.dist + 'styles'));
-
-    // copy lib scripts, maintaining original directory structure
-    // gulp.src(paths.libs, {cwd: 'app/**'})
-    //    .pipe(gulp.dest(bases.dist));
-});
-
-// observes file changes
-gulp.task('watch', function() {
-    gulp.watch('app/**/*', ['scripts', 'copy']);
-});
-
-// define the default sequence of tasks
-// gulp.task('default', ['clean:dist', 'scripts', 'imgmin', 'copy']);
-gulp.task('default', ['clean:dist', 'scripts', 'copy']);
+//
+// gulp.task('lint', function() {
+//     return gulp.src(paths.scripts, {cwd: bases.app})
+//               // eslint() attaches the lint output to the eslint property
+//               // of the file object so it can be used by other modules.
+//               .pipe(eslint())
+//               // eslint.format() outputs the lint results to the console.
+//               // Alternatively use eslint.formatEach() (see Docs).
+//               .pipe(eslint.format())
+//               // To have the process exit with an error code (1) on
+//               // lint error, return the stream and pipe to failOnError last.
+//               .pipe(eslint.failOnError());
+// });
+//
+// // process scripts & concatenate into output file
+// gulp.task('scripts', ['clean'], function() {
+//     gulp.src(paths.scripts, {cwd: bases.app})
+//         .pipe(eslint())
+//         .pipe(eslint.format())
+//         .pipe(uglify())
+//         .pipe(concat('app.min.js'))
+//         .pipe(gulp.dest(bases.dist + 'scripts/'));
+// });
+//
+// // imagemin minimizes images and outputs to dist
+// // gulp.task('imgmin', ['clean'], function() {
+// //     gulp.src(paths.images, {cwd: bases.app})
+// //         .pipe(imgmin())
+// //         .pipe(gulp.dest(bases.dist + 'images/'));
+// // });
+//
+// // copy all other files to dist directly
+// gulp.task('copy', ['clean'], function() {
+//     // copy html
+//     gulp.src(paths.html, {cwd: bases.app})
+//         .pipe(gulp.dest(bases.dist));
+//
+//     // copy styles
+//     gulp.src(paths.styles, {cwd: bases.app})
+//         .pipe(gulp.dest(bases.dist + 'styles'));
+//
+//     // copy lib scripts, maintaining original directory structure
+//     // gulp.src(paths.libs, {cwd: 'app/**'})
+//     //    .pipe(gulp.dest(bases.dist));
+// });
+//
+// // observes file changes
+// gulp.task('watch', function() {
+//     gulp.watch('app/**/*', ['scripts', 'copy']);
+// });
+//
+// // define the default sequence of tasks
+// // gulp.task('default', ['clean:dist', 'scripts', 'imgmin', 'copy']);
+// gulp.task('default', ['clean:dist', 'scripts', 'copy']);
